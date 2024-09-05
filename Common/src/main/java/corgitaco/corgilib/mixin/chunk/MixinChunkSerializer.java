3df5a881dad1bc7e5e2201ce5eab55e8d63c0861
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2024-09-05 00:22:30 -0700

    Serialize the right info for scheduled random ticks

--- Common/src/main/java/corgitaco/corgilib/mixin/chunk/MixinChunkSerializer.java
+++ Common/src/main/java/corgitaco/corgilib/mixin/chunk/MixinChunkSerializer.java
@@ -45,7 +45,7 @@ public class MixinChunkSerializer {
         if (tag.contains("corgilib")) {
             CompoundTag corgiLibTag = tag.getCompound("corgilib");
             if (corgiLibTag.contains("scheduled_random_ticks", Tag.TAG_LIST)) {
-                for (Tag scheduledTick : tag.getList("scheduled_random_ticks", Tag.TAG_COMPOUND)) {
+                for (Tag scheduledTick : corgiLibTag.getList("scheduled_random_ticks", Tag.TAG_COMPOUND)) {
 
                     if (scheduledTick instanceof IntArrayTag tag1) {
                         ((RandomTickScheduler) cir.getReturnValue()).getScheduledRandomTicks().add(new BlockPos(tag1.get(0).getAsInt(), tag1.get(1).getAsInt(), tag1.get(2).getAsInt()));
