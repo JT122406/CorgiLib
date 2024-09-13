846bdfd581e6dd187f163d4f8e2ad589eee964ee
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-09-13 15:28:54 -0400

    Remove unused CommonSetupEvent from NeoForge

--- NeoForge/src/main/java/dev/corgitaco/corgilib/neoforge/CorgiLibNeoForge.java
+++ NeoForge/src/main/java/dev/corgitaco/corgilib/neoforge/CorgiLibNeoForge.java
@@ -4,7 +4,6 @@ import corgitaco.corgilib.CorgiLib;
 import dev.corgitaco.corgilib.neoforge.platform.NeoForgePlatform;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.registries.DataPackRegistryEvent;
 import net.neoforged.neoforge.registries.NewRegistryEvent;
 
@@ -14,15 +13,9 @@ import net.neoforged.neoforge.registries.NewRegistryEvent;
 @Mod(CorgiLib.MOD_ID)
 public class CorgiLibNeoForge {
     public CorgiLibNeoForge(IEventBus modEventBus) {
-        modEventBus.addListener(this::commonSetup);
         CorgiLib.init();
         NeoForgePlatform.CACHED.values().forEach(deferredRegister -> deferredRegister.register(modEventBus));
         modEventBus.addListener(DataPackRegistryEvent.NewRegistry.class, newRegistry -> NeoForgePlatform.DATAPACK_REGISTRIES.forEach(newRegistryConsumer -> newRegistryConsumer.accept(newRegistry)));
         modEventBus.addListener(NewRegistryEvent.class, newRegistry -> NeoForgePlatform.NEW_REGISTRIES.forEach(newRegistryConsumer -> newRegistryConsumer.accept(newRegistry)));
     }
-
-
-    private void commonSetup(final FMLCommonSetupEvent event) {
-    }
-
 }
